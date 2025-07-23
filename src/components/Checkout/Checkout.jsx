fac2616a73ba61b83adefb9339f40fdd6ffc26b4
Anas26772001 <anassawah267@gmail.com>
2025-07-23 16:57:32 +0300

    Handle path after Online Payment

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -14,7 +14,8 @@ export default function Checkout({ totalPrice }) {
     //  ^ Pay Online
     async function payOnline(values) {
         try {
-            const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://e-commerce-app-nine-rho.vercel.app/`,
+            const forwardUrl = `https://e-commerce-app-nine-rho.vercel.app//allorders`
+            const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${forwardUrl}`,
                 { shippingAddress: values },
                 { headers: { token: localStorage.getItem('token') } })
             if (data.status == 'success') {
