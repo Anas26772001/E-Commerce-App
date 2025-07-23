ef62e2a7f012dede18bdef05ede93e2664854233
Anas26772001 <anassawah267@gmail.com>
2025-07-23 17:18:41 +0300

    Handle Path After Online Payment

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -18,7 +18,7 @@ export default function Checkout({ totalPrice }) {
                 { shippingAddress: values },
                 { headers: { token: localStorage.getItem('token') } })
             if (data.status == 'success') {
-                window.location.href = 'https://e-commerce-app-nine-rho.vercel.app/allorders'
+                window.location.href = data.session.url
             }
         } catch (error) {
             console.log(error.message)
